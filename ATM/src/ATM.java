a7e1037855ef26ad8d87a49e82531743dfc45181
Khaled <Khaled@DESKTOP-JN79S56>
2019-03-15 16:56:49 +0200

    First Commit

---
+++ ATM/src/ATM.java
@@ -0,0 +1,55 @@
+
+public class ATM {
+	private String IdNum = "1000";;
+	private String password = "1000";
+	private float CurrentBalance;
+
+	
+
+	ATM() {
+		this.CurrentBalance = (float) 0.0;
+
+	}
+
+	ATM(float CurrentBalance) {
+		this.CurrentBalance = CurrentBalance;
+	}
+
+	public float Withdraw(int cash) {
+    if(cash>this.CurrentBalance && cash<50 && (cash%10>0) )
+       {
+           return 0;
+					      }
+	 else{
+				                  this.CurrentBalance-=cash;
+					          return CurrentBalance;
+					          }
+    }
+
+	
+
+	public void setCurrentBalance(float currentBalance) {
+		CurrentBalance = currentBalance;
+	}
+
+	public float getCurrentBalance() {
+		return CurrentBalance;
+	}
+
+	public float Deposit(int cash) {
+		
+			this.CurrentBalance += cash;
+			return CurrentBalance;
+		
+	}
+
+	public  boolean validate(String IdNum, String password) {
+		return (this.IdNum.equals(IdNum) && this.password.equals(password));
+	}
+	public  boolean isNumericArray(String str) { 
+	    for (char c : str.toCharArray())
+	        if (c < '0' || c > '9')
+	            return false;
+	    return true;}
+
+}
